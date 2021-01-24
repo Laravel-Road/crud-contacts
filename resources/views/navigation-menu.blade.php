3202bf59d37da104e2d04add89b9daa4e7296534
Pedro Augusto <pams.pedro@gmail.com>
2021-01-24 02:03:04 -0300

    cria rota e navegacao para contatos

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -15,6 +15,10 @@
                     <x-jet-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-jet-nav-link>
+
+                    <x-jet-nav-link href="{{ route('contacts.index') }}" :active="request()->routeIs('contacts.index')">
+                        {{ __('Contacts') }}
+                    </x-jet-nav-link>
                 </div>
             </div>
 
@@ -140,6 +144,10 @@
         <div class="pt-2 pb-3 space-y-1">
             <x-jet-responsive-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
+            </x-jet-responsive-nav-link>
+
+            <x-jet-responsive-nav-link href="{{ route('contacts.index') }}" :active="request()->routeIs('contacts.index')">
+                {{ __('Contacts') }}
             </x-jet-responsive-nav-link>
         </div>
 
